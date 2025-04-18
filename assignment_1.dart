fa1d18d4ea125ac45112c08d048b0d91e2e047b9
Shahrukh-khan27 <Skhan200427@gmail.com>
2025-04-18 22:04:11 +0500

    Assignment 1

---
+++ assignment_1.dart
@@ -0,0 +1,20 @@
+void main() {
+  
+  print ("This is Shahrukh. I am undergraduate from virtual university");
+
+  String stdname = "shahrukh khan";
+  print (stdname);
+
+  int age = 21;
+  print (age);
+
+  var university = "Virtual University";
+  print (university);
+
+  String inter = "Intermediate Result";
+  print (inter);
+
+  double percentage = 55;
+  print (percentage);
+
+}
